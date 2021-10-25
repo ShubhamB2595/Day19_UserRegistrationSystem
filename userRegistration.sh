375b7f0d7bb6fdd38898904ef480bc36b92a3867
ShubhamB2595 <shubhambhele95@gmail.com>
2021-10-25 16:08:32 +0530

    refactored for getting valid last name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,12 +1,23 @@
 #! /bin/bash -x
 
 echo "Welcome to User Registration System Program"
-namePat="^[A-Z]{1}[a-zA-Z]{2,}$"
+firstNamePat="^[A-Z]{1}[a-zA-Z]{2,}$"
+lastNamePat="^[A-Z]{1}[a-zA-Z]{2,}$"
+
 echo "Enter the user's First Name"
 read firstName
-if [[ $firstName =~ $namePat ]]
+if [[ $firstName =~ $firstNamePat ]]
 then
-	echo "$firstName is Valid name"
+	echo "$firstName is Valid first name"
 else
-	echo "$firstName is Invalid name"
+	echo "$firstName is Invalid first name"
+fi
+
+echo "Enter the user's Last Name"
+read lastName
+if [[ $lastName =~ $lastNamePat ]]
+then
+        echo "$lastName is Valid last name"
+else
+        echo "$lastName is Invalid last name"
 fi
