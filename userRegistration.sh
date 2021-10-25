7ab42eb55c36ccbc23bc6446963abc80600b0812
ShubhamB2595 <shubhambhele95@gmail.com>
2021-10-25 16:02:27 +0530

    refactored for checking validation of User's first name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,3 +1,12 @@
 #! /bin/bash -x
 
-echo Weclome to User registration System Program
+echo "Welcome to User Registration System Program"
+namePat="^[A-Z]{1}[a-zA-Z]{2,}$"
+echo "Enter the user's First Name"
+read firstName
+if [[ $firstName =~ $namePat ]]
+then
+	echo "$firstName is Valid name"
+else
+	echo "$firstName is Invalid name"
+fi
